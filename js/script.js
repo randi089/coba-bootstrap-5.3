8608883507940ce7a0c033f7296d13e8d5d4ffcc
Randi Febriadi <randifebriadi@gmail.com>
2024-07-31 13:05:44 +0700

    menambahkan fitur submit form

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Script Contact Form
+const scriptURL = "https://script.google.com/macros/s/AKfycbzCOmb8cRBfZfxpqI-uIcET-xad7k9ZJ3L5dLELzpVU-6Tocuv8VQo042PWju-bKG0pRw/exec";
+const form = document.forms["ranfeb-contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+const myClose = document.querySelector(".my-close");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //   Ketika tombol submit diklik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      //   tampilkan alert
+      myAlert.classList.toggle("d-none");
+      //   reset form
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
+// alert close di klik
+myClose.addEventListener("click", () => {
+  myAlert.classList.toggle("d-none");
+});
