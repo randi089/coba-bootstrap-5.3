95fed2b03795b79555ab2244a2e47d21f0003133
Randi Febriadi <randifebriadi@gmail.com>
2025-01-23 10:34:46 +0700

    selesai

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Script Contact Form
-const scriptURL = "https://script.google.com/macros/s/AKfycbzCOmb8cRBfZfxpqI-uIcET-xad7k9ZJ3L5dLELzpVU-6Tocuv8VQo042PWju-bKG0pRw/exec";
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzCOmb8cRBfZfxpqI-uIcET-xad7k9ZJ3L5dLELzpVU-6Tocuv8VQo042PWju-bKG0pRw/exec";
 const form = document.forms["ranfeb-contact-form"];
 const btnKirim = document.querySelector(".btn-kirim");
 const btnLoading = document.querySelector(".btn-loading");
@@ -36,3 +37,83 @@ VanillaTilt.init(document.querySelectorAll(".dream-box"), {
   speed: 1000,
   glare: true,
 });
+
+// Dark Mode
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2024 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+(() => {
+  "use strict";
+
+  const getStoredTheme = () => localStorage.getItem("theme");
+  const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
+
+  const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme();
+    if (storedTheme) {
+      return storedTheme;
+    }
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  };
+
+  const setTheme = (theme) => {
+    if (theme === "auto") {
+      document.documentElement.setAttribute(
+        "data-bs-theme",
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light"
+      );
+    } else {
+      document.documentElement.setAttribute("data-bs-theme", theme);
+    }
+  };
+
+  setTheme(getPreferredTheme());
+
+  const showActiveTheme = (theme) => {
+    const activeThemeIcon = document.querySelector(".theme-icon-active");
+    const btnToActive = document.querySelector(
+      `[data-bs-theme-value="${theme}"]`
+    );
+    const iconOfActiveBtn = btnToActive.querySelector("i").dataset.themeIcon;
+
+    document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
+      element.classList.remove("active");
+      element.setAttribute("aria-pressed", "false");
+    });
+
+    btnToActive.classList.add("active");
+    btnToActive.setAttribute("aria-pressed", "true");
+    activeThemeIcon.classList.remove(activeThemeIcon.dataset.themeIconActive);
+    activeThemeIcon.classList.add(iconOfActiveBtn);
+    activeThemeIcon.dataset.themeIconActive = iconOfActiveBtn;
+  };
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", () => {
+      const storedTheme = getStoredTheme();
+      if (storedTheme !== "light" && storedTheme !== "dark") {
+        setTheme(getPreferredTheme());
+      }
+    });
+
+  window.addEventListener("DOMContentLoaded", () => {
+    showActiveTheme(getPreferredTheme());
+    document.querySelectorAll("[data-bs-theme-value]").forEach((toggle) => {
+      toggle.addEventListener("click", () => {
+        const theme = toggle.getAttribute("data-bs-theme-value");
+        setStoredTheme(theme);
+        setTheme(theme);
+        showActiveTheme(theme);
+      });
+    });
+  });
+})();
